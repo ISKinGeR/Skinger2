9d9187740d3343b19f0bbba3de4c1c592ed5c69e
ISKinGeR <51255680+ISKinGeR@users.noreply.github.com>
2019-06-03 13:18:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const client2 = new Discord.Client();
 const myid = [''];
-const prefix = ['--'];
+const prefix = ['-'];
 
 client.on('ready', () => {
    console.log(`----------------`);
@@ -67,7 +67,6 @@ client.on('message', message => {
   if(command == `قل3`){
    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("no")
    let botmessage = args.join(" ");
-   message.delete().catch;
    message.channel.send(botmessage);
   }
 });
@@ -115,7 +114,6 @@ client2.on('message', message => {
 if(command == `قل4`){
  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("no")
  let botmessage = args.join(" ");
- message.delete().catch;
  message.channel.send(botmessage);
 }
 });
